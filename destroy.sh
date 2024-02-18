66e21e8144b3dd91830db192a826021a2864ec0b
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-02-18 14:30:34 +0100

    Use declared Mullvad version in destroy script

--- destroy.sh
+++ destroy.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 ./down.sh
 echo "Destroying mullvad-proxy images..."
-docker rmi mullvad:latest mvpn-http mvpn-socks5 mvpn-proxy caligari/privoxy:latest
+
+ENVFILE=.env
+if [ -f "$ENVFILE" ]; then
+    source $ENVFILE
+fi
+docker rmi "mullvad:${MULLVAD_VERSION:-latest}" mvpn-http mvpn-socks5 mvpn-proxy caligari/privoxy:latest
