db2ebdff948ec4da6a098b6b42a832ece93e3340
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-02-18 14:31:28 +0100

    Also remove volumes and orphan containers in destroy script

--- destroy.sh
+++ destroy.sh
@@ -6,4 +6,5 @@ ENVFILE=.env
 if [ -f "$ENVFILE" ]; then
     source $ENVFILE
 fi
+docker compose down --volumes --remove-orphans
 docker rmi "mullvad:${MULLVAD_VERSION:-latest}" mvpn-http mvpn-socks5 mvpn-proxy caligari/privoxy:latest
