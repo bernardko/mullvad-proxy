2dbbcfa7a75269a16dcad90fb86feb6978219a0f
Ivan Mirić <1009277+imiric@users.noreply.github.com>
2024-02-18 14:15:19 +0100

    Fix mullvad CLI commands for v2023.6

--- down.sh
+++ down.sh
@@ -2,6 +2,6 @@
 echo "Tearing down mullvad-proxy containers..."
 echo "Getting account info and wireguard key..."
 docker exec -it mvpn mullvad account get
-docker exec -it mvpn mullvad tunnel wireguard key check
-docker-compose down
-echo "Remember to delete the above wireguard keys from the account to free them up."
+docker exec -it mvpn mullvad tunnel get
+docker compose down
+echo "Remember to delete the above Wireguard keys from the account to free them up."
--- setup.sh
+++ setup.sh
@@ -23,13 +23,13 @@ if [ -n "$ACCOUNT" ]; then
     echo "Setting Mullvad Account Number: $ACCOUNT"
     docker exec -it mvpn mullvad status
     docker exec -it mvpn mullvad account get
-    docker exec -it mvpn mullvad account set $ACCOUNT
+    docker exec -it mvpn mullvad account login $ACCOUNT
     docker exec -it mvpn mullvad relay set tunnel-protocol wireguard
-    docker exec -it mvpn mullvad always-require-vpn set on
+    docker exec -it mvpn mullvad lockdown-mode set on
     docker exec -it mvpn mullvad lan set allow
     docker exec -it mvpn mullvad auto-connect set on
     echo "Waiting for Mullvad API Connection..." && sleep 7
-    docker exec -it mvpn mullvad tunnel wireguard key regenerate
+    docker exec -it mvpn mullvad tunnel set wireguard rotate-key
     docker exec -it mvpn mullvad connect
     echo "Waiting for Connection Status..." && sleep 7
     docker exec -it mvpn mullvad status
