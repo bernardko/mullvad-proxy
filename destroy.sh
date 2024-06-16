4cc8f7ac6ca271872bd39f3b2fcf031eef094adc
Bernard Ko <bernardko@gmail.com>
2024-06-16 01:17:10 -0400

    Fixed typox

--- destroy.sh
+++ destroy.sh
@@ -6,4 +6,4 @@ ENVFILE=.env
 if [ -f "$ENVFILE" ]; then
     source $ENVFILE
 fi
-docker rmi "mullvad:${MULLVAD_VERSION:-latest}" mvpn-socks5 mvpn-proxy caligari/privoxy:latest vimagick/delegated:latest
+docker rmi "mullvad:${MULLVAD_VERSION:-latest}" mvpn-socks5 mvpn-proxy caligari/privoxy:latest
