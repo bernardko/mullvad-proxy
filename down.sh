252e3f396630551abae70ca5ef43a93da7600600
Anthony Mugendi <ngurumugz@gmail.com>
2024-09-18 05:51:43 +0300

    Added Logic to cleanup created device
    - Gets device created by the docker process
    - Removes device

--- down.sh
+++ down.sh
@@ -1,7 +1,25 @@
 #! /bin/bash
 echo "Tearing down mullvad-proxy containers..."
 echo "Getting account info and wireguard key..."
-docker exec -it mvpn mullvad account get
+
+# Get all account & devices
+account="$(docker exec -it mvpn mullvad account get)"
+devices="$(docker exec --tty mvpn mullvad account list-devices | tail -n +2)"
+
+echo $account
 docker exec -it mvpn mullvad tunnel get
+
+# loop till we get this device
+while IFS= read -r device; do
+    # echo "device $device"
+    found="$(echo "$account" | grep "$device")"
+    if [ -n "$found" ]; then
+        deviceStr="$(echo "${device//[$'\t\r\n']/}")"
+        echo "Removing device '$deviceStr'"
+        # remove device
+        docker exec --tty mvpn mullvad account revoke-device "$deviceStr"
+
+    fi
+done <<<"$devices"
+
 docker compose down --volumes --remove-orphans
-echo "Remember to delete the above Wireguard keys from the account to free them up."
